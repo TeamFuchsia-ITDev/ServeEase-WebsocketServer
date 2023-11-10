d04cb8beacbbabba833ac8807c19352d70779ace
RazielGelo <angeloguerra1986x@gmail.com>
2023-11-09 18:16:35 -0800

    testing another version

--- server.mjs
+++ server.mjs
@@ -32,8 +32,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("locationMessage", (room, message) => {
-	console.log("MESSAGE", message)
-    io.to(room).emit("location", `${socket.username} ${message.role} ${message.lat} ${message.lng}`);
+    io.to(room).emit("location", `${message.lat} ${message.lng}`);
   });
 
   // Listen for typing events
